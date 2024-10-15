503176478c53f63d650ab72ae461d1e6025b93f1
Alvaro Sevilla Gutiérrez <70824325+alvarosevilla2@users.noreply.github.com>
2024-10-15 16:52:13 -0500

    Update app4.py

--- app4.py
+++ app4.py
@@ -35,11 +35,53 @@ class DoubleConv(nn.Module):
     def forward(self, x):
         return self.conv(x)
 
-class UNet(nn.Module):
+# --- Definición de la clase Down ---
+class Down(nn.Module):
+    """Downscaling with maxpool then double conv"""
+
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.maxpool_conv = nn.Sequential(
+            nn.MaxPool2d(2),
+            DoubleConv(in_channels, out_channels)
+        )
+
+    def forward(self, x):
+        return self.maxpool_conv(x)
+
+# --- Definición de la clase Up ---
+class Up(nn.Module):
+    """Upscaling then double conv"""
+
+    def __init__(self, in_channels, out_channels, bilinear=True):
+        super().__init__()
+
+        # if bilinear, use the faster conv transpose, else use the transposed conv
+        if bilinear:
+            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+            self.conv = DoubleConv(in_channels, out_channels)
+        else:
+            self.up = nn.ConvTranspose2d(in_channels // 2, in_channels // 2, kernel_size=2, stride=2)
+            self.conv = DoubleConv(in_channels, out_channels)
+
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+        # input is CHW
+        diffY = x2.size()[2] - x1.size()[2]
+        diffX = x2.size()[3] - x1.size()[3]
+
+        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
+                        diffY // 2, diffY - diffY // 2])
+        
+        x = torch.cat([x2, x1], dim=1)
+        return self.conv(x)
+        
+
+class UNet(nn.Module): # La definición de UNet debe ir DESPUÉS de Down y Up
     def __init__(self, n_channels=4, n_classes=3):
         super(UNet, self).__init__()
         self.inc = DoubleConv(n_channels, 64)
-        self.down1 = Down(64, 128)
+        self.down1 = Down(64, 128) # Ahora Down está definida
         self.down2 = Down(128, 256)
         self.down3 = Down(256, 512)
         self.down4 = Down(512, 512)
